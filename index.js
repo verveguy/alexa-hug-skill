aa2dc07e9e8eac27a258d1d3bf72bf386af11c3f
Brett Adam <bpjadam@gmail.com>
2017-05-04 08:55:58 +1000

    Cleaning up states somewhat, adding unhandled handlers.

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ exports.handler = function (event, context, callback) {
   let alexa = Alexa.handler(event, context)
   alexa.appId = appId
   alexa.dynamoDBTableName = 'hugger'
-  alexa.registerHandlers(newSessionHandlers, configureHandlers)
+  alexa.registerHandlers(newSessionHandlers, hugHandlers, configureHandlers)
   alexa.execute()
 }
 
 const states = {
-  HUGMODE: '_HUGMODE', // User is trying to hug someone
+  HUGMODE: '_HUGMODE',
   CONFIGUREMODE: '_CONFIGUREMODE'  // Configuration setup mode
 }
 
@@ -56,19 +56,42 @@ const removeHugsForHuggee = function (hugs, huggee) {
 
 var newSessionHandlers = {
   'NewSession': function () {
+    console.log('new session for event:', this.event)
     if (Object.keys(this.attributes).length === 0)
     {
       this.attributes['endedSessionCount'] = 0
       this.attributes['hugsGiven'] = 0
       this.attributes['hugs'] = []
     }
-    this.handler.state = states.STARTMODE
+    this.handler.state = states.HUGMODE
     this.emit(':ask', 'Welcome to Hugger. You have given ' +
       this.attributes['hugsGiven'].toString() + ' hugs. Would you like to hug someone?',
       'Say yes or no to quit.')
   },
 
-  HugIntent: function () {
+  'AMAZON.YesIntent': function () {
+    console.log('YES in empty state')
+    this.handler.state = states.HUGMODE
+    this.emit(':ask', 'Great! Who would you like to hug?', 'Who would you like to hug?')
+  },
+
+  'AMAZON.NoIntent': function () {
+    console.log('NO in empty state')
+    this.handler.state = ''
+    this.emit(':tell', 'Goodbye!')
+  },
+
+  'Unhandled': function () {
+    console.log('Unhandled during initial state', this.event)
+    var message = 'Unhandled intent during initial state.'
+    this.emit(':tell', message)
+  }
+
+}
+
+var hugHandlers = Alexa.CreateStateHandler(states.HUGMODE, {
+  'GiveHugIntent': function () {
+    console.log('GiveHugIntent', this.event)
     const huggee = getSlot(this.event, 'Huggee')
     const hugger = getSlot(this.event, 'Hugger')
     if (typeof hugger === 'undefined')
@@ -85,7 +108,8 @@ var newSessionHandlers = {
     }
   },
 
-  CheckHugsIntent: function () {
+  'CheckHugsIntent': function () {
+    console.log('CheckHugsIntent', this.event)
     let huggee = getSlot(this.event, 'Huggee')
     if (typeof huggee === 'undefined')
     {
@@ -100,7 +124,8 @@ var newSessionHandlers = {
     }
   },
 
-  CountHugsIntent: function () {
+  'CountHugsIntent': function () {
+    console.log('CountHugsIntent', this.event)
     let huggee = getSlot(this.event, 'Huggee')
     if (typeof huggee === 'undefined')
     {
@@ -115,7 +140,8 @@ var newSessionHandlers = {
     }
   },
 
-  RetrieveHugsIntent: function () {
+  'RetrieveHugsIntent': function () {
+    console.log('RetrieveHugsIntent', this.event)
     let huggee = getSlot(this.event, 'Huggee')
     if (typeof huggee === 'undefined')
     {
@@ -141,7 +167,8 @@ var newSessionHandlers = {
     }
   },
 
-  ConfigureIntent: function () {
+  'ConfigureIntent': function () {
+    console.log('ConfigureIntent', this.event)
     this.handler.state = states.CONFIGUREMODE
     let message = 'Configure your hugger by saying "Register Person eating Food with Animal".'
     this.emit(':ask', message, message)
@@ -149,26 +176,39 @@ var newSessionHandlers = {
 
   'AMAZON.StopIntent': function ()
   {
+    console.log('StopIntent', this.event)
     this.emit(':tell', 'Goodbye!')
   },
 
   'AMAZON.CancelIntent': function () {
+    console.log('CancelIntent', this.event)
     this.emit(':tell', 'Goodbye!')
   },
 
   'SessionEndedRequest': function () {
-    console.log('session ended!')
+    console.log('SessionEndedRequest', this.event)
     // this.attributes['endedSessionCount'] += 1
+    this.handler.state = ''
+    this.emit(':saveState', true)
     this.emit(':tell', 'Goodbye!')
+  },
+
+  'Unhandled': function () {
+    console.log('Unhandled during HUGMODE', this.event)
+    var message = 'Unhandled intent during HUGMODE state.'
+    this.emit(':tell', message)
   }
-}
+
+})
 
 var configureHandlers = Alexa.CreateStateHandler(states.CONFIGUREMODE, {
-  'NewSession': function () {
+  'NewSession': function () { 
+    console.log('NewSession during CONFIGUREMODE', this.event)
     this.emit('NewSession') // Uses the handler in newSessionHandlers
   },
 
   'RegisterIntent': function () {
+    console.log('RegisterIntent during CONFIGUREMODE', this.event)
     const food = getSlot(this.event, 'Food')
     const animal = getSlot(this.event, 'Animal')
     const person = getSlot(this.event, 'Person')
@@ -177,41 +217,54 @@ var configureHandlers = Alexa.CreateStateHandler(states.CONFIGUREMODE, {
       'Say yes to confirm or no to cancel.')
   },
 
+  'GiveHugIntent': function () {
+    console.log('GiveHugIntent during CONFIGUREMODE', this.event)
+    this.handler.state = ''
+    this.emit('GiveHugIntent')
+  },
+
   'AMAZON.HelpIntent': function () {
+    console.log('HelpIntent during CONFIGUREMODE', this.event)
     var message = 'You can configure your hugger by saying "Register Person eating Food with Animal".'
     this.emit(':ask', message, message)
   },
 
   'AMAZON.YesIntent': function () {
+    console.log('YesIntent during CONFIGUREMODE', this.event)
     this.attributes['registration'] = this.attributes['pendingRegistration']
-    this.handler.state = states.HUGMODE
+    this.handler.state = ''
     this.emit(':ask', 'Great! ' + 'Try hugging someone now.', 'Try hugging someone.')
   },
 
   'AMAZON.NoIntent': function () {
-    console.log('NOINTENT')
+    console.log('NoIntent during CONFIGUREMODE', this.event)
+    this.handler.state = ''
     this.emit(':tell', 'Ok, registration unchanged.')
   },
 
   'AMAZON.StopIntent': function () {
-    console.log('STOPINTENT')
+    console.log('StopIntent during CONFIGUREMODE', this.event)
+    this.handler.state = ''
     this.emit(':tell', 'Goodbye!')
   },
 
   'AMAZON.CancelIntent': function () {
-    console.log('CANCELINTENT')
+    console.log('CancelIntent during CONFIGUREMODE', this.event)
+    this.handler.state = ''
     this.emit(':tell', 'Goodbye!')
   },
 
   'SessionEndedRequest': function () {
-    console.log('SESSIONENDEDREQUEST')
+    console.log('SessionEndedRequest during CONFIGUREMODE', this.event)
     // this.attributes['endedSessionCount'] += 1
+    this.handler.state = ''
+    this.emit(':saveState', true)
     this.emit(':tell', 'Goodbye!')
   },
 
   'Unhandled': function () {
-    console.log('UNHANDLED')
-    var message = 'Say yes to continue, or no to end cancel registration.'
+    console.log('Unhandled during CONFIGUREMODE', this.event)
+    var message = 'Say yes to continue, or no to cancel registration.'
     this.emit(':ask', message, message)
   }
 })
